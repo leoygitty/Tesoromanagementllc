9961afc779ab5f5810bdb5a1e59cc4e2b57b48a1
leoygitty <haydenyori11@gmail.com>
2025-12-10 01:44:04 -0500

    Delete api/api directory

--- api/api/ping.js
+++
@@ -1,7 +0,0 @@
-// api/ping.js
-module.exports = (req, res) => {
-  console.log("🔔 /api/ping called with method:", req.method);
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ ok: true, message: "ping alive" }));
-};
