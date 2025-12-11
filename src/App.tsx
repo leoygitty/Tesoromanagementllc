c288427202f3544241df411cc5cb70c8c56f4648
leoygitty <haydenyori11@gmail.com>
2025-12-11 02:34:54 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1183,31 +1183,6 @@ export default function App() {
   </div>
 </div>
               >
-                <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.8 19.8 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6A19.8 19.8 0 0 1 2.11 4.18 2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.8 12.8 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.8 12.8 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
-              </svg>
-              <a href="tel:+12155310907" className="underline">
-                {BUSINESS.phone}
-              </a>
-            </span>
-            <span className="hidden sm:inline">•</span>
-            <a
-              href={`mailto:${BUSINESS.email}`}
-              className="underline"
-            >
-              {BUSINESS.email}
-            </a>
-            <span className="hidden sm:inline">•</span>
-            <a
-              href={BUSINESS.facebook}
-              target="_blank"
-              rel="noreferrer"
-              className="underline"
-            >
-              Facebook
-            </a>
-          </div>
-        </div>
-      </div>
 
       {/* Main nav */}
       <header
