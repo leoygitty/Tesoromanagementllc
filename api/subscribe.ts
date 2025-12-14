867015aefe197d75817739606a61307721fdbf3f
leoygitty <haydenyori11@gmail.com>
2025-12-13 22:54:06 -0500

    Update subscribe.ts

--- api/subscribe.ts
+++ api/subscribe.ts
@@ -1,12 +1,8 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async function handler(
-  req: VercelRequest,
-  res: VercelResponse
-) {
+export default async function handler(req: any, res: any) {
   if (req.method !== "POST") {
     return res.status(405).send("Method Not Allowed");
   }
