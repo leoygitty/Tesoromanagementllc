ebe21d9611f6250b4f7e648ffed580e0128ae416
leoygitty <haydenyori11@gmail.com>
2025-12-17 22:59:48 -0500

    Update checklist-download.ts

--- api/checklist-download.ts
+++ api/checklist-download.ts
@@ -1,14 +1,6 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default function handler(
-  req: VercelRequest,
-  res: VercelResponse
-) {
+export default function handler(req: any, res: any) {
   const pdfUrl =
     "https://neighborhoodkrew.com/NeighborhoodKrewMovingDayChecklist.pdf";
-
-  // Optional: log email + timestamp here later
-  const { email } = req.query;
 
   return res.redirect(302, pdfUrl);
 }
